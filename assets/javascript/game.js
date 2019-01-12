2252442c1bdc4e833c3f28c238a98026f7f5f156
Kyle Robert <notoriouskro@gmail.com>
2019-01-11 20:39:43 -0800

    onClick

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,58 @@
-var randScore = [19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30];
+var cValue = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'];
+
+var randScore = '';
 var randValue = '';
-var Wins = 0;
-var Losses = 0;
-var TotalScore = 0;
-var CrystalOne = '';
-var CrystalTwo = '';
-var CrystalThree = '';
-var CrystalFour = '';
+var wins = 0;
+var losses = 0;
+var totalScore = 0;
+var crystalOne = '';
+var crystalTwo = [];
+var crystalThree = [];
+var crystalFour = [];
 
 
 
-function crystalValue() {
+function initScore() {
     var x = document.getElementById('scoreWins')
-    x.innerHTML = ('Score: ' + Math.floor((Math.random() * 120) + 19));
-  }
+    x.innerHTML = (Math.floor((Math.random() * 120) + 19));
+  };
+
     
+  $(".music1").on("click", function() {
+
+    // Clicking the button triggers an alert message.
+    alert("You clicked a music 1!");
+
+  });
+
+  $(".music2").on("click", function() {
+
+    // Clicking the button triggers an alert message.
+    alert("You clicked a music 2!");
+
+  });
+
+  $(".music3").on("click", function() {
+
+    // Clicking the button triggers an alert message.
+    alert("You clicked a music 3!");
+
+  });
+
+  $(".music4").on("click", function() {
+
+    // Clicking the button triggers an alert message.
+    alert("You clicked a music 4!");
+
+  });
+  
+
+    
+
+  
+
+ 
+
 
 
 
