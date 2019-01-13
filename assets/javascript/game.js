a9c9fcc5d9f32c95d6da9d426518c3ee89870a49
Kyle Robert <notoriouskro@gmail.com>
2019-01-12 19:16:23 -0800

    Update Functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,45 +11,59 @@ var compNum = 0;
 function initScore() {
   compNum = (Math.floor((Math.random() * 120) + 19));
   $('#scoreWins').html(compNum);
+  totalScore = 0;
 
-  ;
-  
 
-  for (var i=0; i<4; i++) {
+  for (var i = 0; i < 4; i++) {
     var random = (Math.floor((Math.random() * 12) + 1));
     crystalVal.push(random);
   }
-console.log(crystalVal);
-};
+ 
+
+}
+
+
+initScore();
 
 
 $(".music").on("click", function () {
   var index = this.id;
 
-  val = crystalVal[index];
+  var val = crystalVal[index];
   console.log(val);
- 
+
 
   totalScore += val;
   console.log(totalScore);
   $('#usrScore').text(totalScore);
-  ;
-
+  doGame();
 
 });
 
-if (totalScore === compNum) {
-wins++;
+function doGame() {
 
-} else if (totalScore > compNum){
-  losses++
-} else (){
+  if (totalScore === compNum) {
+    wins++;
+    $('#win').text("Wins: " + wins);
+    initScore();
 
-};
+  } else if (totalScore > compNum) {
+    losses++;
+    $('#loss').text("Losses: " + losses);
+    initScore();
+  } else  {
+
+  $('#win').text("Wins: " + wins);
+  $('#loss').text("Losses: " + losses);
+  $('#usrScore').text(totalScore);
+}
+}
 
 
 
-initScore();
+
+
+
 
 
 
